efccc694619fc75a21df49c983067899e09031bf
bucketli <zylicfc@gmail.com>
2024-08-26 11:38:44 +0800

    fix(ds):fix scripts bug

--- all_build.sh
+++ all_build.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 
-./gradlew -x test clean build
-./gradlew -x test prepare
-./gradlew -x test dist
+SUB_PROJECT=${1}
+
+if [ "${SUB_PROJECT}" == "" ] ; then
+  echo "Need pass sub project name."
+  exit 1
+elif [ "${SUB_PROJECT}" == "es-trigger-es7" ] ; then
+  echo "For es-trigger-es7 , need Java version >= 1.8"
+elif [ "${SUB_PROJECT}" == "es-trigger-es8" ]; then
+  echo "For es-trigger-es8 , need Java version >= 17"
+else
+  echo "Not supported sub project name:${SUB_PROJECT}"
+  exit 1
+fi
+
+echo "Current JAVA_HOME is:${JAVA_HOME}"
+
+./gradlew -x test -p ${SUB_PROJECT} clean build
+./gradlew -x test -p ${SUB_PROJECT} prepare
+./gradlew -x test -p ${SUB_PROJECT} dist
